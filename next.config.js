fb2077102d65e7f446555e2f440341c0a56fd56d
MarwenTlili <marwentlili@gmail.com>
2024-06-13 15:42:46 +0100

    Mutating Data

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000', '0.0.0.0:3000'],
+    },
+  },
+};
 
 module.exports = nextConfig;
